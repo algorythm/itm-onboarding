d856efd23fc9292a519994e326d0893bb6d228d3
awoitm <awo@it-minds.dk>
2019-02-11 13:28:35 +0100

    Add list of todos fetched from server

---
+++ src/ClientApp/components/Todos/Todos.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <h1>Todos</h1>
+    <div class="loading" v-if="todos.length === 0">Loading todos...</div>
+    <div class="todos" v-if="todos.length > 0">
+        <ul class="todo-container" v-for="todo in todos" :key="todo.id">
+            <li>{{todo.title}}</li>
+        </ul>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todos: []
+        }
+    },
+    mounted() {
+        console.log("Fetching todos");
+        fetch("/api/todo")
+            .then(res => res.json())
+            .then(data => {
+                console.log("DONE!")
+                console.log("Loaded todos from API", data);
+                this.todos = data;
+            });
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
