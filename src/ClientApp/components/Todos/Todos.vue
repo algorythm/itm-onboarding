9eebf7d63c30e2b6679568b115f8bea56e312fcf
awoitm <awo@it-minds.dk>
2019-02-11 15:31:06 +0100

    Basic list structure

--- src/ClientApp/components/Todos/Todos.vue
+++ src/ClientApp/components/Todos/Todos.vue
@@ -4,7 +4,7 @@
     <div class="loading" v-if="todos.length === 0">Loading todos...</div>
     <div class="todobox" v-if="todos.length > 0">
         <ul class="todo-list">
-            <li class="todo-item" v-for="todo in todos" :key="todo.id">
+            <li :class="[completedClass(todo), 'todo-item']" v-for="todo in todos" :key="todo.id">
                 <p>{{todo.title}}</p>
             </li>
         </ul>
@@ -29,11 +29,60 @@ export default {
         addTodo() {
             this.$store.dispatch("todos/add", todo);
         },
+        completedClass(todo) {
+            return todo.completed ? "todo-completed" : "";
+        }
     }
 }
 </script>
 
 <style scoped>
+.todobox {
+    min-width: 500px;
+    padding: 12px 0;
 
+    background: #fff;
+    border-radius: 2px;
+    display: inline-block;
+    margin: 1rem;
+    position: relative;
+
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+}
+
+.todo-item {
+    margin: 0;
+    position: table-cell;
+    vertical-align: middle;
+}
+
+.todo-item:hover {
+    background-color: rgba(0, 0, 0, .12);
+}
+
+.todo-item p {
+    margin: auto;
+    padding: 16px;
+    display: inline-flex;
+}
+
+.todo-item::after {
+    content: "";
+    display: block;
+    border-top: solid 1px rgba(0, 0, 0, .12);
+    padding: 0 !important;
+    margin: 0;
+}
+
+.todo-list {
+    margin: 0;
+    padding: 0;
+}
+
+.todo-completed {
+    text-decoration: line-through;
+    color: rgba(0, 0, 0, .54);
+}
 </style>
 
