3c6c0eab90924a788616eb4ab8dafd71ad21af4c
awoitm <awo@it-minds.dk>
2019-02-11 13:27:17 +0100

    Add required annotations to DTO for input validation

--- src/Services/TodoServices/TodoListDto.cs
+++ src/Services/TodoServices/TodoListDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace todoProject.Services.TodoServices
 {
     public class TodoListDto
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public bool Completed { get; set; }
     }
 }
