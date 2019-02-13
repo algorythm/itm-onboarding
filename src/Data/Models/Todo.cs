50a715a34d444945bef5ba116b283e303862b504
awoitm <awo@it-minds.dk>
2019-02-13 13:57:29 +0100

    User id is string, not int

--- src/Data/Models/Todo.cs
+++ src/Data/Models/Todo.cs
@@ -7,7 +7,7 @@ namespace todoProject.Data.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public bool Done { get; set; } = false;
-        public int OwnerId { get; set; }
+        public string OwnerId { get; set; }
         public virtual IdentityUser Owner { get; set; }
     }
 }
